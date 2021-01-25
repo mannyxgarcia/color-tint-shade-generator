e8602d263bafdd130a7d63274fb89961795ef0c3
Manny Garcia <mannyalgarcia@gmail.com>
2021-01-24 16:30:34 -0800

    Feature- Added copy to clipboard to SingleColor.js and Compliment.js

--- src/Compliment.js
+++ src/Compliment.js
@@ -3,17 +3,30 @@ import './main.css';
 
 
 export const Compliment = ({color}) => {
+  const [alert, setAlert] = useState(false)
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 2500)
+    return () => clearTimeout(timeout)
+  }, [alert])
   return (
     <>
     <div 
       className='color'
-      style={{ backgroundColor: `${color.compliment}`}}>
+      style={{ backgroundColor: `${color.compliment}`}}
+      onClick={() => {
+        setAlert(true)
+        navigator.clipboard.writeText(color.hex)
+      }}
+      >
       <div className={color.index < 4 ? 'selected-dark-hex' : 'selected-light-hex'}>
         {color.compliment}
       </div>
       <div className={color.index < 4 ? 'selected-dark' : 'selected-light'}>
         {color.primary ? 'C' : null}
       </div>
+      {alert && <div className={color.index < 4 ? 'selected-dark-alert' : 'selected-light-alert'}> copied to clipboard </div>}
     </div>
   </>
   )
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,11 +2,22 @@ import React, { useState, useEffect } from 'react'
 import './main.css';
 
 export const SingleColor = ({index, color}) => {
+  const [alert, setAlert] = useState(false)
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 2500)
+    return () => clearTimeout(timeout)
+  }, [alert])
   return (
     <>
       <div 
         className='color'
         style={{ backgroundColor: `${color.hex}`}}
+        onClick={() => {
+          setAlert(true)
+          navigator.clipboard.writeText(color.hex)
+        }}
         >
         <div className={color.index < 4 ? 'selected-dark-hex' : 'selected-light-hex'}>
           {color.hex}
@@ -14,6 +25,7 @@ export const SingleColor = ({index, color}) => {
         <div className={color.index < 4 ? 'selected-dark' : 'selected-light'}>
         {color.primary ? 'P' : null}  
         </div>
+        {alert && <div className={color.index < 4 ? 'selected-dark-alert' : 'selected-light-alert'}> copied to clipboard </div>}
       </div>
     </>
   )
